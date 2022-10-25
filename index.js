816e2fcd2c8830b68dc563c8d72a9761d5eac098
Sujoy Paul <freelancersujoy@gmail.com>
2022-10-25 18:50:56 +0600

    api link generate succesfull

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const express = require(express);
+const express = require('express');
 const app = express()
-
 const port = 5000;
+const cors = require('cors')
+app.use(cors())
+
+const category = require('./data/category.json');
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
     res.send("Hurraa! This code is running")
 })
 
+app.get('/categories', (req, res) => {
+    res.send(category)
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const getCategory = courses.find(c => c._id == id);
+    res.send(getCategory)
+})
+
+
 app.listen(port, () => {
-    console.log(`Deep-coder-knowledge server is running at: ${port}`);
+    console.log(`deep-coder-knowledge server is running at: ${port}`);
 })
